f021e63fe57fadef9596d0fa4386996544fd3629
jclagoria <lagoria79@gmail.com>
2024-02-23 12:20:08 -0300

    Add classes for work with files yaml

--- src/main/java/ar/com/api/categories/configuration/ApiServiceConfig.java
+++ src/main/java/ar/com/api/categories/configuration/ApiServiceConfig.java
@@ -1,2 +1,14 @@
-package ar.com.api.categories.configuration;public class ApiServiceConfig {
+package ar.com.api.categories.configuration;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "coins")
+public class ApiServiceConfig {
+
+    private String baseUrl;
+    private String healthApi;
+
 }
--- src/main/java/ar/com/api/categories/configuration/ExternalServerConfig.java
+++ src/main/java/ar/com/api/categories/configuration/ExternalServerConfig.java
@@ -1,2 +1,15 @@
-package ar.com.api.categories.configuration;public class ExternalServerConfig {
+package ar.com.api.categories.configuration;
+
+import lombok.Getter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "api")
+@Getter
+public class ExternalServerConfig {
+
+    private String urlCoinGecko;
+    private String ping;
+
 }
